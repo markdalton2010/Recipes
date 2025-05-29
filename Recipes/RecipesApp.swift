817c8ce0f3cc82b3359abfd20b85e47f916978e8
Mark Dalton <markdalton2010@gmail.com>
2025-05-29 14:13:11 -0500

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Mark Dalton on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipesTests/RecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesTests.swift
+//  RecipesTests
+//
+//  Created by Mark Dalton on 5/29/25.
+//
+
+import Testing
+@testable import Recipes
+
+struct RecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
